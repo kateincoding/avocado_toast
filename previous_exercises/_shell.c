d1b69958f5d7a8deb873b30e3237fd9c7cf8408b
Your Name <k.soto@pucp.pe>
2021-04-04 15:52:56 -0500

    last update -not finished

--- previous_exercises/_shell.c
+++ previous_exercises/_shell.c
@@ -3,6 +3,51 @@
 #include <string.h>
 #include <unistd.h>
 
+
+void	*_memalloc(size_t size)
+{
+	unsigned char *usize;
+
+	usize = malloc(size);
+	if (usize == NULL)
+		return (NULL);
+	while (size)
+		usize[--size] = 0;
+	return ((void *)usize);
+}
+
+void	*_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	idx;
+
+	idx = 0;
+	if (!src && !dst)
+		return (NULL);
+	while (idx < n)
+	{
+		((unsigned char*)dst)[idx] = ((unsigned char*)src)[idx];
+		++idx;
+	}
+	return (dst);
+}
+
+
+void create_argvs(char **array, char *line)
+{
+	char *next_argv = strchr(line, ' ');
+	unsigned int i, j = 0;
+
+	for(i = 0; line[i], i++)
+	{
+		if (line [i] == ' ')
+		{
+			array[j][i] = _memalloc(i);
+			*array[j] = _memcpy(&array[j], line, i - 1);
+			/*falta*/
+		}
+	}
+}
+
 /*ft to return number of ' ' in a function */
 int count_spaces(char *str)
 {
@@ -16,12 +61,15 @@ int count_spaces(char *str)
 }
 
 /* ft to execute the child process with a line like as input */
+/* paso 2 cambiar la fx, cuando sea un input por la tuberia */
 void execute_child(char *line)
 {
 	int len_line = strlen(line);
 	/*falta agregar a la fx que elimine los espacios extra y si hay al final */
 	int _argc = count_spaces(line) + 1;
 	char *_argv[_argc];
+	/* fx que aloca los argv en el array */
+	create_argvs(*_argv, line);
 	printf ("execute function execute_child well \n");
 }
 
@@ -41,17 +89,17 @@ int main()
 		/* it can return -1 if it is failure */
 		if (read == -1)
 			exit(100);
-		else 
-		{
-			puts("escribe una linea aqui /*entro*/");
-			printf("el valor de line es = %s", line);
-			printf("el valor de read get line  es = %d\n", read);
-		}
-		if (read == EOF || strcmp(line,"exit\n") == 0)
+		else if (read == EOF || strcmp(line,"exit\n") == 0)
 		{
 			free(line);
 			puts("salimos con eof o exit101");
-			exit(100);
+			exit (0);
+		}
+		else 
+		{
+			puts("escribe una linea aqui /*entro*/");
+			printf("el valor de line es = %s\n", line);
+			printf("el valor de read get line  es = %d\n", read);
 		}
 
 		pid = fork();
